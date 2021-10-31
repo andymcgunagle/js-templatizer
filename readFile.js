4733b8912fe64e0f9d0641e9ad800d030af9ceff
andymcgunagle <amcgunagle@gmail.com>
2021-10-31 12:25:35 -0700

    Figured out how to write before and after the file is read and re-written line by line.

--- readFile.js
+++ readFile.js
@@ -2,26 +2,37 @@
 import readline from 'readline';
 import fs from 'fs';
 
-const templatize = (fileToTemplatize, replaceThisString, withThisString) => {
+const templatize = (fileToTemplatize, replaceThese) => {
   const rl = readline.createInterface({
     input: fs.createReadStream(fileToTemplatize),
   });
 
-  const writeStream = fs.createWriteStream(`${withThisString}.js`, { encoding: "utf8" })
+  const fileName = fileToTemplatize.split('.')[0];
+
+  const writeStream = fs.createWriteStream(`${fileName}Generator.js`, { encoding: "utf8" });
+
+  writeStream.write('HELLO!\n');
 
   rl.on('line', (line) => {
     // let newLine = `\`${line}\` +${EOL}`;
     let newLine = `\`${line}\` +\n`;
 
-    if (line.includes(replaceThisString)) {
-      newLine = newLine.replace(replaceThisString, `\${${withThisString}}`);
+    for (let object of replaceThese) {
+      const wordToReplace = Object.keys(object)[0];
+
+      if (line.includes(wordToReplace)) {
+        newLine = newLine.replace(wordToReplace, `\${${object[wordToReplace]}}`);
+      };
     };
 
     if (line !== '') {
       writeStream.write(newLine);
     };
-
   });
-}
 
-templatize('userModel.js', 'user', 'chicken');
+  rl.on('close', () => {
+    writeStream.write('GOODBYE!');
+  });
+};
+
+templatize('userModel.js', [{ 'user': 'chicken' }, { 'User': 'Chicken' }]);
